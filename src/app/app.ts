0f8fb593c3cc38fc81007d15d8cd82213b04deb2
Abdelrahman El Kilany <abamo22@gmail.com>
2025-07-19 14:50:25 +0300

    using angular material as standalone instead of modules

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,10 @@
 import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MaterialModuleModule } from './material-module/material-module-module';
 import { AuthService } from './services/auth-service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MaterialModuleModule],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- src/app/components/login/login.ts
+++ src/app/components/login/login.ts
@@ -8,12 +8,27 @@ import {
 } from '@angular/forms';
 import { AuthService } from '../../services/auth-service';
 import { ActivatedRoute, Router } from '@angular/router';
-import { MaterialModuleModule } from '../../material-module/material-module-module';
 import { take } from 'rxjs';
+import { MatButton, MatButtonModule } from '@angular/material/button';
+import { MatCard, MatCardActions, MatCardContent, MatCardHeader, MatCardSubtitle, MatCardTitle } from '@angular/material/card';
+import { MatError, MatFormField, MatLabel } from '@angular/material/form-field';
+import { MatInput } from '@angular/material/input';
 
 @Component({
   selector: 'app-login',
-  imports: [CommonModule, ReactiveFormsModule, MaterialModuleModule],
+  imports: [CommonModule, ReactiveFormsModule,
+    MatButtonModule,
+    MatFormField,
+    MatLabel,
+    MatButton,
+    MatError,
+    MatInput,
+    MatCard,
+    MatCardTitle,
+    MatCardHeader,
+    MatCardActions,
+    MatCardContent,
+    MatCardSubtitle],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
